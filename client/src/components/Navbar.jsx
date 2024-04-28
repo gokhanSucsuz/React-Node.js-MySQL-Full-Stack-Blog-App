00f2f2d8f11689c0f92655a49ae5890c73145290
Gökhan SUÇSUZ <coolpisces22@gmail.com>
2024-04-28 15:12:56 +0300

    style file is writing...

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,14 +1,14 @@
-
+import Logo from "../img/logo.png"
 
 const Navbar = () => {
     return (
         <div className="navbar">
             <div className="container">
                 <div className="logo">
-
+                    <img src={Logo} alt="logo" />
                 </div>
                 <div className="links">
-
+                    links
                 </div>
             </div>
 
