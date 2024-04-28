acb4ea79ab141143896432c669617cd4a566cba8
Gökhan SUÇSUZ <coolpisces22@gmail.com>
2024-04-28 22:14:21 +0300

    ReactQuill editor is setup

--- client/src/pages/Write.jsx
+++ client/src/pages/Write.jsx
@@ -1,7 +1,23 @@
+import { useState } from 'react';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
 
 const Write = () => {
+  const [value, setValue] = useState('');
   return (
-    <div>Write</div>
+    <div className="add">
+      <div className="content">
+
+        <input type="text" placeholder="Title" />
+        <div className="editorContainer">
+          <ReactQuill theme="snow" value={value} onChange={setValue} />;
+        </div>
+      </div>
+      <div className="menu">
+        <div className="item">item1</div>
+        <div className="item">item2</div>
+      </div>
+    </div>
   )
 }
 
