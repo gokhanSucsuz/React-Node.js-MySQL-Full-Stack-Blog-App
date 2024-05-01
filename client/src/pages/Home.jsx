7ebe052c1f920f3520524b90ae0cc3c317f02933
Gökhan SUÇSUZ <coolpisces22@gmail.com>
2024-05-01 15:59:00 +0300

    right menu is done

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -34,7 +34,6 @@ export const Home = () => {
   const [posts, setPosts] = useState([])
 
   const cat = useLocation().search;
-  console.log(cat)
 
   useEffect(() => {
     const fetchData = async () => {
