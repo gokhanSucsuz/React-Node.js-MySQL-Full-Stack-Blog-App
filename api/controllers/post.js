e8270eb3e54be52f970794f67db4e96a4fb25239
Gökhan SUÇSUZ <coolpisces22@gmail.com>
2024-05-01 14:18:57 +0300

    post category query is added

--- api/controllers/post.js
+++ api/controllers/post.js
@@ -1,3 +1,24 @@
+import { db } from "../db.js";
+
+export const getPosts = (req, res) => {
+	const q = req.query.cat
+		? "select * from posts where cat = ?"
+		: "select * from posts";
+	db.query(q, [req.query.cat], (err, data) => {
+		if (err) return res.send(err);
+		return res.status(200).json(data);
+	});
+};
+
+export const getPost = (req, res) => {
+	res.json("from controller");
+};
 export const addPost = (req, res) => {
+	res.json("from controller");
+};
+export const deletePost = (req, res) => {
+	res.json("from controller");
+};
+export const updatePost = (req, res) => {
 	res.json("from controller");
 };
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -9,7 +9,9 @@ const Navbar = () => {
         <div className="navbar">
             <div className="container">
                 <div className="logo">
-                    <img src={Logo} alt="logo" />
+                    <Link className="link" to="/">
+                        <img src={Logo} alt="logo" />
+                    </Link>
                 </div>
                 <div className="links">
                     <Link className="link" to="/?cat=art"><h6>ART</h6></Link>
