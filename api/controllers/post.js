c0b080ea52d12fd2f4d1f1ff9ee355404351b890
Gökhan SUÇSUZ <coolpisces22@gmail.com>
2024-05-01 15:46:26 +0300

    post page is done

--- api/controllers/post.js
+++ api/controllers/post.js
@@ -1,23 +1,42 @@
 import { db } from "../db.js";
+import jwt from "jsonwebtoken";
 
 export const getPosts = (req, res) => {
 	const q = req.query.cat
 		? "select * from posts where cat = ?"
 		: "select * from posts";
 	db.query(q, [req.query.cat], (err, data) => {
-		if (err) return res.send(err);
+		if (err) return res.status(500).send(err);
 		return res.status(200).json(data);
 	});
 };
 
 export const getPost = (req, res) => {
-	res.json("from controller");
+	const q =
+		"select `username`,`title`,`desc`,u.img,p.img as postImg,`cat`,`date` from users u join posts p on u.id = p.uid where p.id = ?";
+	db.query(q, [req.params.id], (err, data) => {
+		console.log(req.params.id);
+		if (err) return res.status(500).send(err);
+		console.log(data);
+		return res.status(200).json(data);
+	});
 };
 export const addPost = (req, res) => {
 	res.json("from controller");
 };
 export const deletePost = (req, res) => {
-	res.json("from controller");
+	const token = req.cookies.access_token;
+	if (!token) return res.status(401).json("Not authenticated!");
+
+	jwt.verify(token, "jwtkey", (err, userInfo) => {
+		if (err) return res.status(403).json("Token is not valid!");
+	});
+
+	const q = "delete from posts where `id` = ?and `uid` = ?";
+	db.query(q, [req.params.id, userInfo.id], (err, data) => {
+		if (err) return res.status(403).json("You can delete only your post!");
+		return res.status(200).json("Post has been deleted!");
+	});
 };
 export const updatePost = (req, res) => {
 	res.json("from controller");
--- client/src/pages/Single.jsx
+++ client/src/pages/Single.jsx
@@ -1,30 +1,61 @@
 import Edit from "../img/edit.png"
 import Delete from "../img/delete.png"
-import { Link } from "react-router-dom"
+import { Link, useLocation, useNavigate } from "react-router-dom"
 import Menu from "../components/Menu"
+import { useContext, useEffect, useState } from "react"
+import axios from "axios"
+import moment from "moment"
+import { AuthContext } from "../context/authContext"
 
 const Single = () => {
+  const [post, setPost] = useState([])
+  const navigate = useNavigate()
+  const { currentUser } = useContext(AuthContext)
+  const location = useLocation();
+  const postId = location.pathname.split("/")[2]
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(`http://localhost:8080/api/posts/${postId}`)
+        setPost(res.data[0])
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    fetchData();
+  }, [postId])
+
+  const handleDelete = async () => {
+    try {
+      await axios.delete(`http://localhost:8080/api/posts/${postId}`)
+      navigate("/")
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  console.log("currentUser: ", currentUser?.username, "postUser: ", post?.username)
+
   return (
     <div className="single">
       <div className="content">
-        <img src="https://images.pexels.com/photos/7008010/pexels-photo-7008010.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
+        <img src={post?.postImg} alt="" />
         <div className="user">
-          <img src="https://images.pexels.com/photos/7008010/pexels-photo-7008010.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
+          <img src={post?.img} alt="" />
           <div className="info">
-            <span>John</span>
-            <p>Posted 2 days ago</p>
+            <span>{post?.username}</span>
+            <p>Posted {moment(post.date).fromNow()}</p>
           </div>
-          <div className="edit">
-            <Link to={`/write?edit=2`}><img src={Edit} alt="" /></Link>
-            <img src={Delete} alt="" />
-          </div>
+          {currentUser?.username === post?.username && (<div className="edit">
+            <Link to={`/write?edit=2`}>
+              <img src={Edit} alt="" />
+            </Link>
+            <img onClick={handleDelete} src={Delete} alt="" />
+          </div>)}
         </div>
-        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Suscipit placeat omnis expedita dignissimos veritatis vitae quia fuga fugit excepturi itaque provident quaerat dicta reprehenderit quo, ex mollitia architecto, molestiae quidem hic.
-          <br /><br />
-          Pariatur, libero repellat, nihil qui aliquid eaque, recusandae quo excepturi voluptatem dolores asperiores! Libero quaerat minus ullam, facere harum voluptate dignissimos repellat eaque sed, beatae unde. At in fugiat aut, illo praesentium impedit ipsam repudiandae voluptates nihil porro consectetur sunt velit. Blanditiis voluptatum laborum explicabo consequuntur voluptate quas esse voluptas magnam? Atque, non natus! Maiores, odit, molestiae porro asperiores, reprehenderit provident tempore nam rerum quas inventore architecto ipsum incidunt.
-          <br /><br />
-          Blanditiis dolor illum deserunt reiciendis ea ad optio officiis sapiente hic, quod veritatis ullam cumque officia incidunt modi dolores impedit vero. Ratione, sint amet. Voluptatem ut, aut aliquid quaerat dicta earum placeat perferendis quia! Nulla voluptatem possimus deserunt, dolorum, assumenda officiis qui debitis impedit unde porro reiciendis sequi dolores quos?</p>
+        <h1>{post.title}</h1>
+        {post.desc}
       </div>
       <Menu />
     </div>
