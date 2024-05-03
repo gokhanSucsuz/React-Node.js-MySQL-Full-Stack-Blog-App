4f1801d791e339ad6755bcfb279405ae9a7f4df0
Gökhan SUÇSUZ <coolpisces22@gmail.com>
2024-05-03 23:11:06 +0300

    changessssss....

--- api/index.js
+++ api/index.js
@@ -20,6 +20,16 @@ const corsOptions = {
 	optionsSuccessStatus: 200,
 };
 
+app.use((req, res, next) => {
+	res.header(
+		"Access-Control-Allow-Origin",
+		"https://react-node-js-my-sql-full-stack-blog-app.vercel.app/"
+	);
+	res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+	res.header("Access-Control-Allow-Headers", "Content-Type");
+	next();
+});
+
 //app.options("*", cors());
 app.use(cors(corsOptions));
 
