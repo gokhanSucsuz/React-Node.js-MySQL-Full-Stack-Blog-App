337d109027d63da099679f8cbfff3c29d049d5d2
Gökhan SUÇSUZ <coolpisces22@gmail.com>
2024-05-03 22:41:03 +0300

    index.js change

--- api/index.js
+++ api/index.js
@@ -20,6 +20,13 @@ const corsOptions = {
 	optionsSuccessStatus: 200,
 };
 
+app.use((req, res, next) => {
+	res.header("Access-Control-Allow-Origin", "React app URL");
+	res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+	res.header("Access-Control-Allow-Headers", "Content-Type");
+	next();
+});
+
 app.options("*", cors());
 app.use(cors(corsOptions));
 
