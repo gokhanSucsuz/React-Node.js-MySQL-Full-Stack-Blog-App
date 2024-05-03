34f8a2d0afbd19d05869c6c462c2975f5c4e4511
Gökhan SUÇSUZ <coolpisces22@gmail.com>
2024-05-03 23:10:28 +0300

    changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,12 +6,5 @@ export default defineConfig({
 	plugins: [react()],
 	server: {
 		port: 3000,
-		proxy: {
-			"/api": {
-				target:
-					"https://nodejs-api-react-node-js-my-sql-full-stack-blog-app-ft5n.vercel.app",
-				secure: false,
-			},
-		},
 	},
 });
